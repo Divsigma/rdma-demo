96f4f0968f8c61ab25eebe2de2d98d235a34e056
divsigma <pontsh@163.com>
2023-05-21 21:37:57 +0800

    fix bug: make qp_attr and qp_access_flags valid for rdma-write

--- README.md
+++ README.md
@@ -16,24 +16,26 @@ $ PYTHONPATH=<python_path> ./lo_rc_send.py
 运行示例：
 
 ```shell
-# socket建链：服务端
+# socket建链+send：服务端
 $ chmod +x rdma_rc_test.py
-$ PYTHONPATH=<python_path> ./rdma_rc_test.py --device r0
-# socket建链：客户端
+$ PYTHONPATH=<python_path> ./rdma_rc_test.py --device r0 --mode=send
+# socket建链+send：客户端
 $ chmod +x rdma_rc_test.py
-$ PYTHONPATH=<python_path> ./rdma_rc_test.py --device r0 --server_ip 192.168.56.102
+$ PYTHONPATH=<python_path> ./rdma_rc_test.py --device r0 --mode=send --server_ip 192.168.56.102
 
-# CM建链：服务端
+# CM建链+rdma_write：服务端
 $ chmod +x rdma_rc_test.py
-$ PYTHONPATH=<python_path> ./rdma_rc_test.py --device r0 --cm 1
-# CM建链：客户端
+$ PYTHONPATH=<python_path> ./rdma_rc_test.py --device r0 --cm 1 --mode=write
+# CM建链+rdma_write：客户端
 $ chmod +x rdma_rc_test.py
-$ PYTHONPATH=<python_path> ./rdma_rc_test.py --device r0 --cm 1 --server_ip 192.168.56.102
+$ PYTHONPATH=<python_path> ./rdma_rc_test.py --device r0 --cm 1 --mode=write --server_ip 192.168.56.102
 ```
 
-通过socket或CM建链交换GID、QPN、MR的RKEY、MR的ADDDR，完成rdma操作。
+通过socket或CM建链交换GID、QPN、MR的RKEY、MR的ADDDR，完成send或rdma_write操作。对于send操作，需要两端的post_recv和post_send做好同步；对于rdma_write操作，接收方就不用post_recv了（此时是内核旁路！），但仍需要通过链路（比如conn.handshake()）告知对方操作已完成。
 
-预期效果是服务端能看到类似日志（即服务端的mr_recv数据被修改）：
+注：操作过程中产生的错误会形成一个WC，通过查询WC的状态说明可以帮助定位问题
+
+预期效果是服务端能看到类似日志（即服务端的mr_recv数据被修改），且修改客户端wr_send的opcode为IBV_WR_SEND和IBV_WR_RDMA_WRITE时，抓包可以看到携带客户端信息的报文的BTH中OpCode分别是`Send Only (4)`和`RDMA Write Only (10)`：
 
 ```shell
 [WARNING] [<module>] mr_recv content before: [[Default Empty Recv Data]] - rdma_rc_test.py:114
--- rdma_rc_test.py
+++ rdma_rc_test.py
@@ -15,19 +15,29 @@ import time
 import utils.connection
 from utils.logging_utils import root_logger
 
+def wait_until_one_wc():
+    npolled, wcs = -1, None
+    while npolled <= 0:
+        npolled, wcs = cq0.poll(1)
+    root_logger.info("npolled = {} cqe_status={}".format(npolled, cq.cqe_status_to_str(wcs[0].status)))
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--server_ip', dest='server_ip', type=str, default='')
     parser.add_argument('--device', dest='device_name', type=str, required=True)
     parser.add_argument('--cm', dest='cm', type=str, default=None)
+    parser.add_argument('--mode', dest='mode', type=str, default='send', required=True)
     args = parser.parse_args()
+
+    assert args.mode == 'send' or args.mode == 'write'
 
     # ---- 获取通讯设备名 ----
     lst = device.get_device_list()
 
     RECV_WR_ID = 2021
     SEND_WR_ID = 2017
+    SEND_WR_NOTIFY_ID = 1007
 
     root_logger.info("get lst: {}".format(lst))
 
@@ -55,8 +65,13 @@ if __name__ == "__main__":
     cq0 = cq.CQ(ctx, 100)
     cap0 = qp.QPCap(1, 1)
     # qia0 = qp.QPInitAttr(cap=cap0, qp_type=enums.IBV_QPT_UD, scq=cq0, rcq=cq0)
-    qia0 = qp.QPInitAttr(cap=cap0, qp_type=enums.IBV_QPT_RC, scq=cq0, rcq=cq0)
-    rcqp = qp.QP(pd0, qia0, qp.QPAttr())
+    qia0 = qp.QPInitAttr(cap=cap0, qp_type=enums.IBV_QPT_RC, scq=cq0, rcq=cq0, sq_sig_all=1)
+
+    # bug1：需要设置QP的初始属性
+    #       根据源码，init_attr和qp_attr只用一个就行，而二者兼具时qp_attr会覆盖
+    #       当使用RDMA write操作，而rcqp = qp.QP(pd0, qia0, qp.QPAttr())
+    #       有WC显示WR Flush Error（本端）/remote invalidate request error（对端）
+    rcqp = qp.QP(pd0, qia0)
 
     # ---- 获取本端信息 ----
     # RoCEv2对设备的每个端口维护一张GID表，所以只需要通过gid_index即可获取GID（类似AH）
@@ -79,69 +94,96 @@ if __name__ == "__main__":
     gr0 = addr.GlobalRoute(dgid=dgid0, sgid_index=sgid_index)
     ah_attr0 = addr.AHAttr(port_num=sport_num, is_global=1, gr=gr0)
     ah0 = addr.AH(pd0, attr=ah_attr0)
-    # QP状态转入RTS，该部分挺重要：
-    # 不调用to_rts可能在post_send时有errno=22（为何？）
-    # 不设置ah_attr可能在to_rtr时有errno=22（为何？）
-    # 却可以不设置dest_qp_num，此时无法通过post_send完成mr_recv的修改（为何？）
+
+    # 设置QP属性和状态，这部分闹出了不少bug：
+    #   bug1：不调用to_rts()可能在post_send时有errno=22（为何？）
+    #   bug2：不设置ah_attr可能在执行到源码to_rtr时有errno=22（为何？）
+    #         却可以不设置dest_qp_num，此时无法通过post_send完成mr_recv的修改（为何？）
+    #   bug3：不正确设置qp_access_flags，可能在RDMA操作时，
+    #         有WC显示WR Flush Error（本端）/remote invalidate request error（对端）
+    #         （详见ibv_modify_qp手册说明，事实上pyverbs中to_init方法就调用了ibv_modify_qp）
     qa0 = qp.QPAttr()
     qa0.ah_attr = ah_attr0
+    qa0.qp_access_flags = enums.IBV_ACCESS_REMOTE_WRITE | enums.IBV_ACCESS_LOCAL_WRITE
     qa0.dest_qp_num = remote_node_info['qpn']
     rcqp.to_rts(qa0)
 
     # ---- 准备scatter/gather读写区域 ----
+    # 根据ibv_reg_mr手册：REMOTE_WRITE和LOCAL_WRITE必须同时设置
     mr_send = mr.MR(pd0, 
                     length=50, 
-                    access=enums.IBV_ACCESS_LOCAL_WRITE | enums.IBV_ACCESS_REMOTE_READ)
+                    access=enums.IBV_ACCESS_LOCAL_WRITE)
     send_sge_list = [
         wr.SGE(addr=mr_send.buf, length=mr_send.length, lkey=mr_send.lkey)
     ]
-    # 参考ibv_reg_mr，REMOTE_WRITE和LOCAL_WRITE必须同时设置
     mr_recv = mr.MR(pd0,
                     length=50,
-                    access=enums.IBV_ACCESS_LOCAL_WRITE | enums.IBV_ACCESS_REMOTE_WRITE)
+                    access=enums.IBV_ACCESS_REMOTE_WRITE | enums.IBV_ACCESS_LOCAL_WRITE)
     recv_sge_list = [
         wr.SGE(addr=mr_recv.buf, length=mr_recv.length, lkey=mr_recv.lkey)
     ]
-    # 通过建链获取对端MR的rkey和addr
-    remote_mr_info = conn.handshake(rkey=mr_recv.rkey, addr=mr_recv.buf)
+    # 如果使用RDMA write，通过建链获取对端MR的rkey和addr
+    if args.mode == 'write':
+        root_logger.info("to send rkey={} addr={}".format(mr_recv.rkey, mr_recv.buf))
+        remote_mr_info = conn.handshake(rkey=mr_recv.rkey, addr=mr_recv.buf)
 
     # ---- 开始交换数据 ----
     if is_server:
         # 服务端只管接受
         recv_content_str_data = "[Default Empty Recv Data]"
         mr_recv.write(recv_content_str_data, len(recv_content_str_data))
-        wr_recv = wr.RecvWR(wr_id=RECV_WR_ID, num_sge=len(recv_sge_list), sg=recv_sge_list)
-        root_logger.warning("mr_recv content before: [{}]".format(mr_recv.read(mr_recv.length, offset=0).decode()))
+        root_logger.warning("mr_recv content before: [{}]".format(
+            mr_recv.read(mr_recv.length, offset=0).decode()
+        ))
 
-        # 同步：发送方post_send前、接收方post_recv后
-        # 否则接收方可能无法从CQ中poll出post_recv的CQE
-        # time.sleep(2)
-        rcqp.post_recv(wr_recv)
+        if args.mode == 'send':
+            wr_recv = wr.RecvWR(wr_id=RECV_WR_ID, num_sge=len(recv_sge_list), sg=recv_sge_list)
+            # ---- 同步：接收方需要在发送方post_send之前，post_recv ----
+            # 否则接收方可能无法从CQ中poll出post_recv的WC
+            rcqp.post_recv(wr_recv)
+
         conn.handshake(syn=1)
+
     else:
         # 客户端只管发送
         send_content_str_data = ">>> client saying hello .......... |"
         mr_send.write(send_content_str_data, len(send_content_str_data))
         root_logger.info("mr_send content to send: [{}]".format(mr_send.read(mr_send.length, offset=0).decode()))
-        wr_send = wr.SendWR(wr_id=SEND_WR_ID, opcode=enums.IBV_WR_SEND, num_sge=1, sg=send_sge_list)
-
+       
+        wr_send = None
+        if args.mode == 'send':
+            wr_send = wr.SendWR(wr_id=SEND_WR_ID,
+                                opcode=enums.IBV_WR_SEND,
+                                num_sge=1, sg=send_sge_list)
+        elif args.mode == 'write':
+            wr_send = wr.SendWR(wr_id=SEND_WR_ID,
+                                opcode=enums.IBV_WR_RDMA_WRITE,
+                                num_sge=1, sg=send_sge_list)
+            rkey = remote_mr_info['rkey']
+            raddr = remote_mr_info['addr']
+            root_logger.info("remote rkey=0x{:x}, remote addr=0x{:x}".format(rkey, raddr))
+            wr_send.set_wr_rdma(rkey=rkey, addr=raddr)
+        assert wr_send
+        
         # wr_send.set_wr_ud(ah0, rqpn=udqp.qp_num, rqkey=0)
-        rkey = remote_mr_info['rkey']
-        addr = remote_mr_info['addr']
-        root_logger.info("remote rkey=0x{:x}, remote addr=0x{:x}".format(rkey, addr))
-        wr_send.set_wr_rdma(rkey=rkey, addr=addr)
-        # 同步：发送方post_send前、接收方post_recv后
-        # 否则接收方可能无法从CQ中poll出post_recv的CQE
+        # ---- 同步：接收方需要在发送方post_send之前，post_recv ----
+        # 否则接收方可能无法从CQ中poll出post_recv的WC
         conn.handshake(syn=1)
         rcqp.post_send(wr_send)
 
+
     # 等待接收WR或发送WR完成
-    npolled = -1
-    wcs = None
-    while npolled <= 0:
-        npolled, wcs = cq0.poll()
-    # npolled, wcs = cq0.poll()
-    # time.sleep(1)
+    if is_server:
+        if args.mode == 'send':
+            wait_until_one_wc()
+        elif args.mode == 'write':
+            conn.handshake(done_write=1)
+            root_logger.info("be notified that RDMA write is done")
+    else:
+        wait_until_one_wc()
+        if args.mode == 'write':
+            conn.handshake(done_write=1)
+            root_logger.info("to notify that RDMA write is done")
 
     root_logger.warning("mr_recv content after: [{}]".format(mr_recv.read(mr_recv.length, offset=0).decode()))
 
